33e3dd0f65cc90b29b9636b525759e35b53d633a
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-08 10:38:10 -0600

    Create setup for test and add test for instance

---
+++ test/open_mic_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+require './lib/user'
+require './lib/open_mic'
+
+class UserTest < MiniTest::Test
+  def setup
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+  end
+
+  def test_it_exists
+    assert_instance_of OpenMic, open_mic
+  end
+
+end
