632a7dcec4abc853d9c5332009d495129de32b14
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-08 09:23:54 -0600

    Add test for no jokes and add some jokes to setup

--- test/user_test.rb
+++ test/user_test.rb
@@ -6,10 +6,16 @@ require './lib/user'
 class UserTest < MiniTest::Test
   def setup
     @sal = User.new("Sal")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   def test_it_exists
     assert_instance_of User, @sal
+  end
+
+  def test_no_jokes
+    assert_equal [], @sal.jokes
   end
 
 end
