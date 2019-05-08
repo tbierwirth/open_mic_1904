f60be9e0e403b69d58d6c895a57f3b0e8ffd2766
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-08 10:54:42 -0600

    Add tests for repeated jokes

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -9,6 +9,8 @@ class UserTest < MiniTest::Test
     @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
     @sal = User.new("Sal")
     @ali = User.new("Ali")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   def test_it_exists
@@ -26,6 +28,21 @@ class UserTest < MiniTest::Test
     @open_mic.welcome(@ali)
 
     assert_equal [@sal, @ali], @open_mic.performers
+  end
+
+  def test_for_not_repeated_jokes
+    @ali.learn(@joke_1)
+    @ali.learn(@joke_2)
+
+    refute @open_mic.repeated_jokes?
+  end
+
+  def test_for_repeated_jokes
+    @ali.learn(@joke_1)
+    @ali.learn(@joke_2)
+    @ali.tell(@sal, @joke_1)
+
+    assert @open_mic.repeated_jokes?
   end
 
 end
