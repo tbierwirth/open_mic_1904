91c922d354477ed68ef3f34cdbfce2fba92780c0
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-08 10:45:57 -0600

    Add test for attributes

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -6,11 +6,17 @@ require './lib/open_mic'
 
 class UserTest < MiniTest::Test
   def setup
-    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
   end
 
   def test_it_exists
-    assert_instance_of OpenMic, open_mic
+    assert_instance_of OpenMic, @open_mic
+  end
+
+  def test_attributes_of_open_mic
+    assert_equal "Comedy Works", @open_mic.location
+    assert_equal "11-20-18", @open_mic.date
+    assert_equal [], @open_mic.performers
   end
 
 end
