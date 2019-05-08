aaa4cdeaacffafce376cf5a283898f3f76724eb7
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-08 10:49:50 -0600

    Add test for adding performers

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -7,6 +7,8 @@ require './lib/open_mic'
 class UserTest < MiniTest::Test
   def setup
     @open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    @sal = User.new("Sal")
+    @ali = User.new("Ali")
   end
 
   def test_it_exists
@@ -17,6 +19,13 @@ class UserTest < MiniTest::Test
     assert_equal "Comedy Works", @open_mic.location
     assert_equal "11-20-18", @open_mic.date
     assert_equal [], @open_mic.performers
+  end
+
+  def test_add_performer
+    @open_mic.welcome(@sal)
+    @open_mic.welcome(@ali)
+
+    assert_equal [@sal, @ali], @open_mic.performers
   end
 
 end
